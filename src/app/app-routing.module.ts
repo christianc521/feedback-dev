0e1a588e8889c25ca087baf9d24b9eee16fe9b4a
Albert Luna <93148569+A-Luna05@users.noreply.github.com>
2023-05-16 18:07:17 -0500

    styled routing
    styled routing between components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'auth', component: AuthenticationComponent},
-  { path: 'home', component: TestFetchComponent},
+  { path: '', component: AuthenticationComponent},
+  { path: 'fetch', component: TestFetchComponent},
   { path: 'about', component: AboutComponent},
   { path: 'contact', component: ContactComponent},
   { path: 'settings', component: SettingsComponent}
