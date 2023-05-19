6c857cca035e99f0a1f7e938572f985895606f4d
Joseph Reyes <jreyes1919@gmail.com>
2023-05-19 00:06:35 -0500

    started contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
 
+export class ContactComponent implements OnInit{
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    submitForm() {
+        const message = `Sending message "${this.message}" from ${this.firstname} ${this.lastname} from the email ${this.email}`;
+        alert(message); 
+    }
+    
+
+    firstname!: string;
+    lastname!: string;
+    email!: string;
+    message!: string;
 }
