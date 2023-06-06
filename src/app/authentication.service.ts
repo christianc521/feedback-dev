0ff5a93c88a854f882aa002df65a8a829201eb77
stevenrentas <smrentas@gmail.com>
2023-06-05 21:29:53 -0500

    Implement firestore for saving anthems/songs/users

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,52 +1,97 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import {Router} from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
-
   isLoggedin: boolean = false;
+  userId: string = '';
 
-  constructor(private fireauth: AngularFireAuth, private router : Router) {}
+  constructor(
+    private fireauth: AngularFireAuth,
+    private firestore: AngularFirestore,
+    private router: Router
+  ) {}
 
   login(email: string, password: string) {
-    return this.fireauth.signInWithEmailAndPassword(email, password).then(() => {
-      localStorage.setItem('token', 'true');
-      this.isLoggedin = true;
-      console.log("logged in");
-      this.router.navigate(['/fetch']);
-    }, err => {
-      alert('Incorrect Username or Password');
+    return this.fireauth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        if (user) {
+          const userEmail = user.email;
+          const userRef = this.firestore.collection('users', (ref) =>
+            ref.where('email', '==', userEmail)
+          );
+
+          return userRef
+            .get()
+            .toPromise()
+            .then((querySnapshot) => {
+              if (!querySnapshot.empty) {
+                const userId = querySnapshot.docs[0].id;
+                localStorage.setItem('userId', userId); // Store the user ID
+              } else {
+                throw new Error('User not found');
+              }
+              localStorage.setItem('token', 'true');
+              this.isLoggedin = true;
+              console.log('logged in');
+              this.router.navigate(['/fetch']); // Switch to the fetch component
+            });
+        } else {
+          throw new Error('User not found');
+        }
+      })
+      .catch((err) => {
+        alert('Incorrect Username or Password');
+        this.router.navigate(['/']);
+      });
+  }
+
+  signOut() {
+    return this.fireauth.signOut().then(() => {
       this.router.navigate(['/']);
     });
   }
-  
-  signOut(){
-    return this.fireauth.signOut().then(() => {
-      this.router.navigate(['/'])
-    })
-  }
 
-  createAccount(email: string, password : string, confirmedPass : string){
-    return this.fireauth.createUserWithEmailAndPassword(email,password).then ( () => {
-      alert('Account Created');
-    }, err => {
-      alert('Incorrect Username or Password');
-      this.router.navigate(["/"])
-    })
+  createAccount(email: string, password: string, confirmedPass: string) {
+    return this.fireauth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        if (user) {
+          const userId = this.firestore.createId(); // Generate auto-generated ID
+          const userData = {
+            userId: userId,
+            email: user.email,
+            password: '', // Do not store the password in plaintext
+          };
+
+          // Create user document in Firestore with custom and auto-generated IDs
+          return this.firestore.collection('users').doc(userId).set(userData);
+        } else {
+          throw new Error('User not found');
+        }
+      })
+      .then(() => {
+        alert('Account Created');
+      })
+      .catch((error) => {
+        alert('Incorrect Username or Password');
+        this.router.navigate(['/']);
+      });
   }
 
   isLoggedIn() {
-    if (localStorage.getItem("token") == null) {
+    if (localStorage.getItem('token') == null) {
       this.isLoggedin = false;
       return this.isLoggedin;
-    }
-    else {
+    } else {
       return true;
     }
   }
 }
-
-
--- src/app/test-fetch/test-fetch.component.ts
+++ src/app/test-fetch/test-fetch.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
-import { Observable, of } from 'rxjs';
+import { Observable, from, of } from 'rxjs';
 import { switchMap, map, startWith } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-test-fetch',
   templateUrl: './test-fetch.component.html',
-  styleUrls: ['./test-fetch.component.css']
+  styleUrls: ['./test-fetch.component.css'],
 })
-export class TestFetchComponent implements OnInit{
+export class TestFetchComponent implements OnInit {
   searchCtrl = new FormControl();
   searchResults$: Observable<any[]>;
   details: string = '';
@@ -21,51 +22,59 @@ export class TestFetchComponent implements OnInit{
   songs: any[] = [];
   clientId = '2ee3e6910a654dac86de2886c9aecde1';
   clientSecret = '78e8b2b04c1e458487260a6063202577';
+  description: any;
+  userId: string; // Added userId property
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private firestore: AngularFirestore) {
     this.getAccessToken();
-    this.searchResults$ = this.searchCtrl.valueChanges
-      .pipe(
-        startWith(''),
-        switchMap(term => this.search(term))
-      );
+    this.searchResults$ = this.searchCtrl.valueChanges.pipe(
+      startWith(''),
+      switchMap((term) => this.search(term))
+    );
   }
 
   ngOnInit() {
-    this.searchResults$ = this.searchCtrl.valueChanges
-      .pipe(
-        startWith(''),
-        switchMap(term => this.search(term))
-      );
-  }
+    this.searchResults$ = this.searchCtrl.valueChanges.pipe(
+      startWith(''),
+      switchMap((term) => this.search(term))
+    );
 
+    this.userId = localStorage.getItem('userId'); // Retrieve the user ID from local storage
+    this.retrieveAnthems(); // Retrieve the anthems
+  }
 
   getAccessToken() {
     const headers = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + btoa(this.clientId + ':' + this.clientSecret)
+      Authorization: 'Basic ' + btoa(this.clientId + ':' + this.clientSecret),
     });
 
     const body = new URLSearchParams();
     body.set('grant_type', 'client_credentials');
 
-    this.http.post('https://accounts.spotify.com/api/token', body.toString(), { headers: headers }).subscribe((response: any) => {
-      this.accessToken = response.access_token;
-    });
+    this.http
+      .post('https://accounts.spotify.com/api/token', body.toString(), {
+        headers: headers,
+      })
+      .subscribe((response: any) => {
+        this.accessToken = response.access_token;
+      });
   }
 
-
-  
   search(term: string): Observable<any[]> {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + this.accessToken
+      Authorization: 'Bearer ' + this.accessToken,
     });
 
     if (term) {
-      return this.http.get(`https://api.spotify.com/v1/search?q=${encodeURIComponent(term)}&type=track&limit=5`, { headers: headers })
-        .pipe(
-          map((response: any) => response.tracks.items)
-        );
+      return this.http
+        .get(
+          `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+            term
+          )}&type=track&limit=5`,
+          { headers: headers }
+        )
+        .pipe(map((response: any) => response.tracks.items));
     } else {
       return of([]);
     }
@@ -73,10 +82,12 @@ export class TestFetchComponent implements OnInit{
 
   fetchSongDetails(song: any) {
     this.artist = song.artists[0].name;
-    this.songName = song.name
-    this.albumCover = song.album.images.find((img: any) => img.width === 300).url;
+    this.songName = song.name;
+    this.albumCover = song.album.images.find(
+      (img: any) => img.width === 300
+    ).url;
   }
-  
+
   displayFn(song: any): string {
     return song && song.name ? `${song.name} by ${song.artists[0].name}` : '';
   }
@@ -89,12 +100,90 @@ export class TestFetchComponent implements OnInit{
     const song = {
       artist: this.artist,
       albumCover: this.albumCover,
-      songName: this.songName
+      songName: this.songName,
+      description: this.description,
     };
-    // We're using the spread operator to create a new array. 
-    // This ensures that Angular's change detection will recognize the change and update the view.
-    // This will change once Firestore is implemented
-    this.songs = [song, ...this.songs];
-    console.log(this.songs);
+
+    const anthem = {
+      songId: '',
+      userId: this.userId,
+      description: this.description,
+    };
+
+    this.firestore
+      .collection('songs')
+      .add(song)
+      .then((songRef) => {
+        const songId = songRef.id;
+        anthem.songId = songId;
+
+        this.firestore
+          .collection('anthems')
+          .add(anthem)
+          .then(() => {
+            this.songs = [song, ...this.songs];
+            console.log(this.songs);
+            this.description = '';
+          })
+          .catch((error) => {
+            console.error('Error creating anthem:', error);
+          });
+      })
+      .catch((error) => {
+        console.error('Error creating song:', error);
+      });
+  }
+
+  retrieveAnthems() {
+    const userId = localStorage.getItem('userId');
+    if (!userId) {
+      console.error('User ID not found');
+      return;
+    }
+
+    this.firestore
+      .collection('anthems', (ref) => ref.where('userId', '==', userId))
+      .snapshotChanges()
+      .pipe(
+        switchMap((snaps) =>
+          from(
+            Promise.all(
+              snaps.map(async (snap) => {
+                const anthem: any = snap.payload.doc.data();
+                const id = snap.payload.doc.id;
+                const songId = anthem.songId;
+
+                if (!songId) {
+                  return null;
+                }
+
+                const songSnap = await this.firestore
+                  .collection('songs')
+                  .doc(songId)
+                  .get()
+                  .toPromise();
+                const songData = songSnap.data();
+
+                if (!songData) {
+                  return null;
+                }
+
+                const mergedAnthem = Object.assign({}, anthem, { songData });
+                return { id, ...mergedAnthem };
+              })
+            )
+          )
+        )
+      )
+      .subscribe((songs) => {
+        this.songs = songs
+          .filter((song) => song !== null)
+          .map((song) => ({
+            artist: song.songData.artist,
+            albumCover: song.songData.albumCover,
+            songName: song.songData.songName,
+            description: song.description,
+          }));
+      });
   }
 }
